e6da71e89f62e709ad19c8b227a4fdffacb8962b
sorawitkku <sorawit.n@kkumail.com>
2023-03-22 23:53:04 +0700

    adding Animation and Camera controll

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
+    private Animator anim;
     private Rigidbody2D mybody;
     private SpriteRenderer sp;
     [SerializeField] private float speedX, speedY;
@@ -13,6 +13,7 @@ public class Movement : MonoBehaviour
     {
         mybody = GetComponent<Rigidbody2D>();
         sp = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -26,18 +27,22 @@ public class Movement : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-
+        if ( h == 0 && v == 0)
+        {
+            anim.SetBool("anim", false);
+        }
+        
         if(h > 0)
         {
             mybody.velocity = new Vector2(speedX, mybody.velocity.y);
             sp.flipX = false;
-
+            anim.SetBool("anim", true);
         }
         else if (h < 0)
         {
             mybody.velocity = new Vector2(-speedX, mybody.velocity.y);
             sp.flipX = true;
-
+            anim.SetBool("anim", true);
         }
         else
         {
@@ -47,10 +52,12 @@ public class Movement : MonoBehaviour
         if(v > 0)
         {
             mybody.velocity = new Vector2(mybody.velocity.x, speedY);
+            anim.SetBool("anim", true);
         }
         else if (v < 0)
         {
             mybody.velocity = new Vector2(mybody.velocity.x, -speedY);
+            anim.SetBool("anim", true);
         }
         else
         {
